1cf72cd0c9de066604351429f348744dad781fb2
Miguel C <mkarmona@gmail.com>
2021-01-31 17:08:13 +0000

    refactor: forget adding number-sign = "upright-open"

--- build-custom.bash
+++ build-custom.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-npm run build --ttf::iosevka-ot-dz
-npm run build --ttf::iosevka-ot-dz-mono
-npm run build --ttf::iosevka-ot-dz-term-lig
-npm run build --ttf-unhinted::iosevka-ot-dz
-npm run build --ttf-unhinted::iosevka-ot-dz-mono
-npm run build --ttf-unhinted::iosevka-ot-dz-term-lig
+npm run build -- ttf::iosevka-ot-dz
+npm run build -- ttf::iosevka-ot-dz-mono
+npm run build -- ttf::iosevka-ot-dz-term-lig
+npm run build -- ttf-unhinted::iosevka-ot-dz
+npm run build -- ttf-unhinted::iosevka-ot-dz-mono
+npm run build -- ttf-unhinted::iosevka-ot-dz-term-lig
